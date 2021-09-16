cc2180945b2c46ec63bf0da46550727fa23e037c
kjan00 <82020240+kjan00@users.noreply.github.com>
2021-09-16 21:13:48 +0900

    주석

---
+++ comment/1.py
@@ -0,0 +1,29 @@
+# 조건문 예제
+# egoing
+# 2015
+
+'''
+조건문 예제
+egoing
+2015
+'''
+
+str = '''
+조건문 예제
+egoing
+2015
+'''
+
+print(str)
+
+str2 = '문자열' #줄바꿈시 에러
+
+input = "ab" # user input password
+real_egoing = 11
+#real_k8805 = "ab"
+if real_egoing == input:
+  print("Hello!, egoing")
+#elif real_k8805 == input:
+#  print("Hello!, k8805")
+else:
+  print("Who are you")
---
+++ comment/1.rb
@@ -0,0 +1,24 @@
+=begin
+이 사이가 전부 주석처리
+조건문 예제
+egoing
+2015
+권장되는 방법은 아니다
+샾만 기억하자
+=end
+
+# 조건문 예제
+# egoing
+# 2015 edit > toggle comment
+
+
+input = 33
+real_egoing = 11
+#real_k8805 = "ab"
+if real_egoing == input
+  puts("Hello!, egoing")
+#elsif real_k8805 == input
+#  puts("Hello!, k8805")
+else
+  puts("Who are you")
+end
