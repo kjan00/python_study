0a15bc694666153298314a815f5ac8b8e2e48b01
kjan00 <82020240+kjan00@users.noreply.github.com>
2021-09-23 19:48:36 +0900

    컨테이너

---
+++ Container/1.py
@@ -0,0 +1,10 @@
+print(type('egiong')) #문자열
+name = 'egoing'
+print(name)
+print(type(['egoing', 'leezche', 'graphittie'])) #list
+names = ['egoing', 'leezche', 'graphittie'] #원소
+print(names)
+print(names[1]) #색인
+egoing = ['programmer', 'seoul', 25, False]
+egoing[1] = 'busan'
+print(egoing)
---
+++ Container/1.rb
@@ -0,0 +1,6 @@
+names = ['egoing', 'leezche', 'graphittie'] #Array
+puts(names.class)
+puts(names[0])
+names[0] = 'k8805'
+print(names)
+puts(names)
